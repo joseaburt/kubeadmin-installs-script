e9dddd2edb55ba3eac47847a4b889c314bc026aa
pino0071@gmail.com <pino0071@gmail.com>
2024-04-19 17:38:59 +0200

    install calico

---
+++ install-k8s-network-plugin.sh
@@ -0,0 +1,22 @@
+# If 192.168.0.0/16 is already in use within your network you must select a different pod network CIDR, replacing 192.168.0.0/16 in the above command.
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install Calico
+echo "Install Calico"
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/custom-resources.yaml
+watch kubectl get pods -n calico-system
+
+echo ""
+echo "Remove the taints on the control plane so that you can schedule pods on it"
+# Remove the taints on the control plane so that you can schedule pods on it.
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+echo "It should return the following: node/<your-hostname> untainted"
+
+kubectl get nodes -o wide
