6ef11d1136c7c0e9ea9fa4e748554ef737cbca6e
pino0071@gmail.com <pino0071@gmail.com>
2024-04-19 19:13:08 +0200

    adds header for bash

--- install-k8s-network-plugin.sh
+++ install-k8s-network-plugin.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 # If 192.168.0.0/16 is already in use within your network you must select a different pod network CIDR, replacing 192.168.0.0/16 in the above command.
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 
--- main-install.sh
+++ main-install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Set SELinux in permissive mode (effectively disabling it)
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
