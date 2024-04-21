e0da36a049ad3691e4a059646cc3772fe231b622
pino0071@gmail.com <pino0071@gmail.com>
2024-04-21 22:22:34 +0200

    remove webhook for avoid issue in apply ingress

--- prepare-ingress-nginx.sh
+++ prepare-ingress-nginx.sh
@@ -1,3 +1,5 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/baremetal/deploy.yaml
 
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+
 kubectl get all -n ingress-nginx
